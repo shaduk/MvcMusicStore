daeb01dc3919ac6a4c22f04244becc05b4e354ad
Shad Ullah Khan <shkha@microsoft.com>
2018-06-19 11:34:19 -0700

    Add project files.

---
+++ MvcMusicStore/Controllers/CheckoutController.cs
@@ -0,0 +1,71 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    [Authorize]
+    public class CheckoutController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        const string PromoCode = "FREE";
+        // GET: Checkout
+
+        public ActionResult AddressAndPayment()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddressAndPayment(FormCollection values)
+        {
+            var order = new Order();
+            TryUpdateModel(order);
+
+            try
+            {
+                if(string.Equals(values["PromoCode"], PromoCode, StringComparison.OrdinalIgnoreCase) == false)
+                {
+                    return View(order);
+                }
+                else
+                {
+                    order.Username = User.Identity.Name;
+                    order.OrderDate = DateTime.Now;
+
+                    storeDB.Orders.Add(order);
+                    storeDB.SaveChanges();
+                    var cart = ShoppingCart.GetCart(this.HttpContext);
+                    cart.CreateOrder(order);
+                    return RedirectToAction("Complete", new { id = order.OrderId });
+                }
+            }
+            catch
+            {
+                return View(order);
+            }
+        }
+
+        public string Test()
+        {
+            return "This is the test controller action";
+        }
+
+        public ActionResult Complete(int id)
+        {
+            bool isValid = storeDB.Orders.Any(o => o.OrderId == id &&
+                            o.Username == User.Identity.Name);
+            if(isValid)
+            {
+                return View(id);
+            }
+            else
+            {
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ MvcMusicStore/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using MvcMusicStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+        public ActionResult Index()
+        {
+            var albums = GetTopSellingAlbums(5);
+            return View(albums);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewData["path"] = Request.PhysicalApplicationPath;
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        private List<Album> GetTopSellingAlbums(int count)
+        {
+            // Group the order details by album and return
+            // the albums with the highest count
+            return storeDB.Albums
+                .OrderByDescending(a => a.OrderDetails.Count())
+                .Take(count)
+                .ToList();
+        }
+    }
+}
